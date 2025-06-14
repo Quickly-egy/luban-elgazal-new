3b460cf0870aa0027166ea47bb480b8c819972ed
ahmedhamdy223 <ahmedhamdy@quickly.codes>
2025-06-14 19:09:43 +0300

    ..

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,6 +1,14 @@
-
+import { FaEnvelope } from "react-icons/fa";
+import styles from "./contact.module.css";
 export default function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <section>
+      <div className={`${styles.hero} flex-column`}>
+        <div className={styles.message_icon}>
+            <FaEnvelope className={styles.post_icon}/>
+        </div>
+        <h2 className={styles.contact_address}>تواصل معنا</h2>
+      </div>
+    </section>
+  );
 }
