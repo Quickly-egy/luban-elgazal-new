362ecddd01fdbb7c50c3616f84fc3c4b2006fd02
Quickly Egypt <quickly.dev.eg@gmail.com>
2025-06-25 17:13:51 +0300

    upload

--- src/pages/PackageDetail/PackageDetail.jsx
+++ src/pages/PackageDetail/PackageDetail.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import PackageGallery from '../../components/common/PackageGallery/PackageGallery';
 import ProductInfo from '../../components/ProductDetail/ProductInfo/ProductInfo';
@@ -9,6 +9,31 @@ const PackageDetail = () => {
   const { id } = useParams();
   const { packages } = useProductsStore();
   
+  useEffect(() => {
+    // طباعة كل البيانات الخاصة بالباقات
+    console.log('=== All Packages Data ===');
+    console.log('Total Packages Count:', packages?.length || 0);
+    console.log('All Packages:', packages);
+
+    if (packages?.length > 0) {
+      packages.forEach((pkg, index) => {
+        console.log(`\n=== Package ${index + 1} Details ===`);
+        console.log('ID:', pkg.id);
+        console.log('Name:', pkg.name);
+        console.log('Description:', pkg.description);
+        console.log('Total Price:', pkg.total_price);
+        console.log('Category:', pkg.category);
+        console.log('Is Active:', pkg.is_active);
+        console.log('Main Image URL:', pkg.main_image_url);
+        console.log('Secondary Image URLs:', pkg.secondary_image_urls);
+        console.log('Products:', pkg.products);
+        console.log('------------------------');
+      });
+    } else {
+      console.log('No packages found in the store');
+    }
+  }, [packages]);
+
   // Find the package by id
   const packageData = packages?.find(pkg => pkg.id === parseInt(id));
 
