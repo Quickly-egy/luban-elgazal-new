8d4cdbde2f3b8fef5e17d7b6bc2a59ba74e21957
Quickly Egypt <quickly.dev.eg@gmail.com>
2025-08-23 16:42:21 +0300

    f

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useMemo, Suspense } from 'react';
+import styles from './Home.module.css';
+
+import ImageSlider from '../../components/common/ImageSlider/ImageSlider';
+import ProductCategories from '../../components/common/ProductCategories/ProductCategories';
+
+const WhyChooseUs = React.lazy(() => import('../../components/common/WhyChooseUs/WhyChooseUs'));
+const SpecialOffers = React.lazy(() => import('../../components/common/SpecialOffers/SpecialOffers'));
+const PromoBanners = React.lazy(() => import('../../components/common/PromoBanners/PromoBanners'));
+const HistoryJourney = React.lazy(() => import('../../components/common/HistoryJourney/HistoryJourney'));
+const FeaturedProducts = React.lazy(() => import('../../components/common/FeaturedProducts/FeaturedProducts'));
+const FeaturedPackages = React.lazy(() => import('../../components/common/FeaturedPackages/FeaturedPackages'));
+const StatisticsSection = React.lazy(() => import('../../components/common/StatisticsSection/StatisticsSection'));
+const CustomerReviews = React.lazy(() => import('../../components/common/CustomerReviews/CustomerReviews'));
 
 const Home = () => {
+  const criticalSections = useMemo(() => (
+    <>
+      <ImageSlider />
+      <ProductCategories />
+    </>
+  ), []);
+
   return (
-    <div>
+    <div className={styles.home}>
+      {/* Above-the-fold content */}
+      {criticalSections}
 
- <h1 style={{color:"red" ,fontSize:"70px", textAlign:"center"}}>
-  الموقع بيه عطل وجاري العمل علي اصلاحه
- </h1>
+      {/* Below-the-fold content */}
+      <Suspense fallback={null}><WhyChooseUs /></Suspense>
+      <Suspense fallback={null}><SpecialOffers /></Suspense>
+      <Suspense fallback={null}><HistoryJourney /></Suspense>
+      <Suspense fallback={null}><FeaturedProducts /></Suspense>
+      <Suspense fallback={null}><FeaturedPackages /></Suspense>
+      <Suspense fallback={null}><PromoBanners /></Suspense>
+      <Suspense fallback={null}><StatisticsSection /></Suspense>
+      <Suspense fallback={null}><CustomerReviews /></Suspense>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
